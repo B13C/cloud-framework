b777767948ebda7719783430057a4b922cd9717d
塵子曦 <britton@126.com>
2025-03-03 10:25:49 +0800

    feat 新增数据库特殊字符转义工具函数

--- leaf-base-framework/src/main/java/cn/maple/core/framework/util/GXDBStringEscapeUtils.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/util/GXDBStringEscapeUtils.java
@@ -1,5 +1,7 @@
 package cn.maple.core.framework.util;
 
+import cn.hutool.core.util.StrUtil;
+
 import java.util.Objects;
 import java.util.regex.Pattern;
 
@@ -150,5 +152,64 @@ public class GXDBStringEscapeUtils {
     public static String removeEscapeCharacter(String text) {
         Objects.nonNull(text);
         return text.replaceAll("\"", "").replaceAll("'", "");
+    }
+
+    /**
+     * 转义 SQL 中的特殊字符，处理包括：
+     * <ul>
+     *   <li>反斜杠： "\" 替换为 "\\\\"</li>
+     *   <li>空字符： "\0" 替换为 "\\0"</li>
+     *   <li>换行符： "\n" 替换为 "\\n"</li>
+     *   <li>回车符： "\r" 替换为 "\\r"</li>
+     *   <li>Ctrl+Z（\032）： 替换为 "\\Z"</li>
+     *   <li>单引号： "'" 替换为 "''"（SQL 标准转义）</li>
+     *   <li>双引号： "\"" 替换为 "\\\""</li>
+     * </ul>
+     *
+     * @param input 原始字符串
+     * @return 转义后的字符串
+     */
+    public static String escapeSql(String input) {
+        if (input == null) {
+            return null;
+        }
+        // 注意顺序，先转义反斜杠，否则后续替换可能会出现重复替换的问题
+        String result = input;
+        result = StrUtil.replace(result, "\\", "\\\\");
+        result = StrUtil.replace(result, "\0", "\\0");
+        result = StrUtil.replace(result, "\n", "\\n");
+        result = StrUtil.replace(result, "\r", "\\r");
+        result = StrUtil.replace(result, "\032", "\\Z");
+        // 单引号转义（SQL 标准做法）
+        result = StrUtil.replace(result, "'", "''");
+        // 双引号转义，根据实际需求决定是否需要
+        result = StrUtil.replace(result, "\"", "\\\"");
+        return result;
+    }
+
+    /**
+     * 针对 SQL LIKE 查询的转义方法。
+     * 除了执行常规 SQL 字符转义外，还需要对 LIKE 模式下的通配符进行转义：
+     * <ul>
+     *   <li>% -> ESCAPE字符 + %</li>
+     *   <li>_ -> ESCAPE字符 + _</li>
+     * </ul>
+     * 并且在 SQL 中需要指定 ESCAPE 字符。
+     *
+     * @param input      原始字符串
+     * @param escapeChar 用于转义的字符，一般为 '\'（反斜杠）
+     * @return 转义后的字符串
+     */
+    public static String escapeSqlForLike(String input, char escapeChar) {
+        if (input == null) {
+            return null;
+        }
+        // 先进行常规 SQL 转义
+        String escaped = escapeSql(input);
+        String escStr = String.valueOf(escapeChar);
+        // 对 LIKE 模式下的特殊字符 % 和 _ 进行转义
+        escaped = StrUtil.replace(escaped, "%", escStr + "%");
+        escaped = StrUtil.replace(escaped, "_", escStr + "_");
+        return escaped;
     }
 }
